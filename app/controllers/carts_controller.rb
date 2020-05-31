e5b15b861ff5ade5d9d046cdf582f1408156df67
vagrant <vagrant@localhost.localdomain>
2020-05-31 09:36:34 +0000

    controller_route_finish

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,16 @@
+class CartsController < ApplicationController
+	def index
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	def reset
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update]
+  get 'users/:id/unregister' => 'users#unregister'
+  get 'users/:id/leave' => 'users#leave'
+  
+  resources :delivery_addresses, only: [:index,:edit,:update,:destroy,:create]
+
+  resources :products, only: [:index, :show]
+
+  resources :orders, only: [:index, :show, :new, :update]
+  get 'orders/:id/confirmation' => 'users#confirmation'
+  get 'orders/:id/completation' => 'users#completation'
+  
+  resources :carts, only: [:index, :create, :update, :destroy]
+  get 'carts/reset' => 'users#reset'
+
+
 end
