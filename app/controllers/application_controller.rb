0a9e07530ad78c8c2d61301b354c9ee79402d3e5
vagrant <vagrant@localhost.localdomain>
2020-06-01 04:47:14 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
-    flash[:notice] = "successfully"
-    admins_homes_top_path(resource)
+  case resource
+  when Admin
+    admins_homes_top_path
+  when User
+    products_path
   end
+ end
+ 
 
-  def after_sign_out_path_for(resource) 
-    flash[:notice] = "Signed out successfully."
-    new_admin_session_path
-  end
+
 end
