1dbcf9fd1d14a3c3c0bc5a470430086b0d0b270b
vagrant <vagrant@localhost.localdomain>
2020-06-01 08:30:10 +0000

    products_new_update

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -1,16 +1,27 @@
 class Admins::ProductsController < ApplicationController
+  
   def index
+  	@products = Product.all
   end
 
   def show
   end
 
   def new
+  	@product = Product.new
   end
 
   def create
+  	@product = Product.new(product_params)
+  	@product.save
+  	redirect_to admins_products_path
   end
-
+  
   def edit
   end
+
+ private
+ def product_params
+ 	   params.require(:product).permit(:product_name,:product_price,:category_id,:product_image_id,:product_status,:product_comment)
+ 	end
 end
