364348db2bc5bccff6733f932aaf23ffc0d73068
fujimorichihiro <fujimori.drum@gmail.com>
2020-06-01 05:32:21 +0000

    product-index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,24 @@
 class ProductsController < ApplicationController
-	def index
-	end
-    
-    def show
+  def index
+  	@category = params[:category]
+  	if @category == "0" #一覧表示
+      @products = Product.page(params[:page]).reverse_order
+    elsif @category == "1" #ケーキ
+    	@products = Product.where(category_id: 1)
+    	@products = Product.page(params[:page]).per(8)
+    elsif @category == "2" #焼き菓子
+    	@products = Product.where(category_id: 2)
+    	@products = Product.page(params[:page]).per(8)
+    elsif @category == "3" #プリン
+    	@products = Product.where(category_id: 3)
+    	@products = Product.page(params[:page]).per(8)
+    else #キャンディ
+    	@products = Product.where(category_id: 4)
+    	@products = Product.page(params[:page]).per(8)
     end
+  end
+
+  def show
+  end
 
 end
