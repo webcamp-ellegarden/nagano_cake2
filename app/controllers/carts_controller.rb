617bb5e498c2a29ebea6959945fc319decec49b3
fujimorichihiro <fujimori.drum@gmail.com>
2020-06-01 12:42:47 +0000

    edit cart-view

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,16 +1,32 @@
 class CartsController < ApplicationController
-	def index
-	end
+  # カートページ
+  def index
+    @carts = Cart.where(user_id: current_user.id)
+  end
+  #商品詳細→プルダウンの個数、保存
+  def create
+  end
 
-	def create
-	end
+#個数変更
+  def update
+  	@cart = cart.find(params(:id))
+  	@cart.update(cart_params)
+  	redirect_to cart_path(@cart)
+  end
+#商品1つ削除
+  def destroy
+    cart = Cart.find(params[:id])
+	cart.destroy
+	redirect_back(fallback_location: root_path)
+  end
+#カートを空にする
+  def reset
+  	current_user.carts.destroy_all
+  	redirect_back(fallback_location: root_path)
+  end
 
-	def update
-	end
-
-	def destroy
-	end
-
-	def reset
-	end
+  private
+  def cart_params
+    prams.require(:cart).permit(:number)
+  end
 end
