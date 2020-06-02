717c826b8a63e94c01206562b1854a91c1705ca4
vagrant <vagrant@localhost.localdomain>
2020-06-02 13:40:17 +0000

    add_delivery_post

--- app/controllers/delivery_addresses_controller.rb
+++ app/controllers/delivery_addresses_controller.rb
@@ -1,5 +1,7 @@
 class DeliveryAddressesController < ApplicationController
 	def index
+      @address = DeliveryAddress.new
+      @addresses = DeliveryAddress.all
 	end
 
 	def edit
@@ -12,5 +14,14 @@ class DeliveryAddressesController < ApplicationController
     end
 
     def create
+      @address = DeliveryAddress.new(delivery_address_params)
+      @address.save
+      redirect_to delivery_addresses_path
     end
+
+    private
+    def delivery_address_params
+       params.require(:delivery_address).permit(:portal_code,:prefecture_code,:address_city,:address_street,:address_building,:receiver)
+    end
+
 end
