737b3f48fbcd71cd086e1f6fe18431a3e4a87c17
上野誉司 <isacatoneu@uenohomareshinoMacBook-puro.local>
2020-06-02 14:32:08 +0900

    ユーザー編集追加

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -4,5 +4,22 @@ class Admins::UsersController < ApplicationController
   end
 
   def show
+  	@user =User.find(params[:id])
   end
+  
+  def edit
+  	@user =User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to admins_users_path
+  	else
+      flash[:notice]="error"
+  		render "edit"
+  	end
+  end
+
+
 end
