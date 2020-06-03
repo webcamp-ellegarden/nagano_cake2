d9b47323092f7b3a6883efb0b3f5865de2bf63c6
NoguchiTamami <mikke417@gmail.com>
2020-06-03 14:14:36 +0900

    マイページを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,32 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!
+
 	def show
+		@user = User.find(current_user.id)
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to user_path(@user.id), notice: 'User was successfully created.'
+		else
+			render action: :edit
+		end
 	end
 
 	def unregister
 	end
 
 	def leave
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:family_name,:first_name,:email,:family_name_kana,:first_name_kana,:tel,:postal_code,:prefecture_code,:address_city,:address_street,:address_building,:password,:password_confirmation)
+
 	end
 end
