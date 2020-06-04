c1c62060887227e2b27089db680afc8051bd3a28
vagrant <vagrant@localhost.localdomain>
2020-06-04 02:01:29 +0000

    add_order_memory-show

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -7,4 +7,20 @@ class Admins::OrdersController < ApplicationController
   def show
   	@order = Order.find(params[:id])
   end
+
+  def update
+     @order = Order.find(params[:id])
+     if @order.update(order_params)
+        flash[:notice] = "successfully"
+        redirect_to admins_orders_path(@order)
+      else
+        render :index
+     end
+   end
+
+    private
+     def order_params
+       params.require(:order).permit(:order_status)
+     end
+  
 end
