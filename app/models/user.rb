d9524ac44588dd46079efefb24fdaabff06125c8
NoguchiTamami <mikke417@gmail.com>
2020-06-07 11:04:04 +0900

    退会機能を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,45 +2,49 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
 #アソシエーション----------------------------------------------------------------------
-    has_many :orders, dependent: :destroy
-    has_many :delivery_addresses, dependent: :destroy
+has_many :orders, dependent: :destroy
+has_many :delivery_addresses, dependent: :destroy
   #カート部分--------------------------------
-    has_many :carts
-    has_many :products, through: :carts
+  has_many :carts
+  has_many :products, through: :carts
   #----------------------------------------
 
 #user_status
-    enum user_status:{validity: 0 ,leave: 1 }
+enum user_status:{ 有効: 0 , 退会済: 1 }
+
+def active_for_authentication?
+  super && (self.user_status == "有効" )
+end
 
 
 #  jp_prefecture用メソッド------------------------------------------------------------
-  include JpPrefecture
-  jp_prefecture :prefecture_code
+include JpPrefecture
+jp_prefecture :prefecture_code
 
-  def prefecture_name
-   JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
-  end
+def prefecture_name
+ JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+end
 
-  def prefecture_name=(prefecture_name)
-   self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
-  end
+def prefecture_name=(prefecture_name)
+ self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+end
 # ----------------------------------------------------------------------------------
 
 #住所カラムの連結
-  def user_address
-    [self.prefecture_name, self.address_city, self.address_street, self.address_building].compact.join
-  end
+def user_address
+  [self.prefecture_name, self.address_city, self.address_street, self.address_building].compact.join
+end
 #名前の連結(使い方： @user.user_fullname →　フルネームが表示される。)
-  def user_fullname
-    [self.family_name, self.first_name].compact.join
-  end
+def user_fullname
+  [self.family_name, self.first_name].compact.join
+end
 
 #名前カラムの連結
-  def user_fullname
-    [self.family_name, self.first_name].compact.join
-  end
+def user_fullname
+  [self.family_name, self.first_name].compact.join
+end
 
 end
