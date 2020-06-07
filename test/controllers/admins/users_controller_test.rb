f44cc1e1d45292e277e85e4ffd8091c097861cc4
上野誉司 <isacatoneu@uenohomareshinoMacBook-puro.local>
2020-06-07 16:45:35 +0900

    add_footer

--- test/controllers/admins/users_controller_test.rb
+++ test/controllers/admins/users_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class Admins::UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @admin = admins(:michael)
+    @admin = admin(:michael)
   end
 
   test "login with valid information" do
     get new_admin_session_path
-    post admin_session_path, params: { session: { email:    @user.email,
+    post admin_session_path, params: { session: { email:    @admin.email,
                                           password: 'password' } }
     follow_redirect!
     assert_template 'asmins/home/top'
