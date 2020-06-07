e529ca167f4cdbd436a24efe2b45231d6cb31bce
fujimorichihiro <fujimori.drum@gmail.com>
2020-06-07 06:43:56 +0000

    fix not_null

--- db/migrate/20200607052336_change_column_on_product.rb
+++ db/migrate/20200607052336_change_column_on_product.rb
@@ -3,7 +3,7 @@ class ChangeColumnOnProduct < ActiveRecord::Migration[5.2]
   	change_column_null :products, :category_id, false, 0
   	change_column_null :products, :product_name, false, 0
   	change_column_null :products, :product_price, false, 0
-  	change_column_null :products, :product_image_id, false, 0
+  	change_column_null :products, :product_image_id
   	change_column_null :products, :product_status, false, 0
   	change_column_null :products, :product_comment, false, 0
   end
