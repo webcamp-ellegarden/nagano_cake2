f5db5d0bb6a0b541ab2e2640a5521f5e786cfe93
fujimorichihiro <fujimori.drum@gmail.com>
2020-06-09 03:36:09 +0000

    fix making-status

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -8,18 +8,36 @@ class Admins::OrdersController < ApplicationController
   	@order = Order.find(params[:id])
   end
 
+# ここ---------------------------------------------------------
   def update
-     @order = Order.find(params[:id])
+     @order = Order.find(params[:id]) #params[:id]から注文呼び出し(常にtrue)
      option = params[:option]
-    if option == "1" 
-      @order_detail = OrderDetail.find(order_details_params[:id])
-      @order_detail.update(order_details_params)
-    else     
+    if option == "1"   #製作ステータスの編集だった場合
+      @order_detail = OrderDetail.find(order_details_params[:id]) #注文詳細呼び出し
+      @order_detail.update(order_details_params) #paramsの内容でアップデート
+      if @order_detail.making_status == "製作中"
+        @order.update(order_status: "製作中")
+      elsif @order_detail.making_status == "製作完了"
+        @order.update(order_status: "発送準備中")
+        @order.order_details.each do |order_detail|
+          if order_detail.making_status == "製作中" || order_detail.making_status == "製作待ち"
+            @order.update(order_status: "製作中")
+          end
+        end
+      end
+
+    else
       @order.update(order_params)
+      if @order.order_status == "入金確認"
+        order_details = OrderDetail.where(order_id: @order.id)
+        order_details.each do |order_detail|
+          order_detail.update(making_status: "製作待ち")
+        end
+      end
     end
       redirect_to admins_order_path(@order)
    end
-
+#-----------------------------------------------------------------
     private
      def order_params
        params.require(:order).permit(:order_status)
