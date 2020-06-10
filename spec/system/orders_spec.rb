e4d783ce5c98334708ac4cf4b7e13e26a918384c
vagrant <vagrant@localhost.localdomain>
2020-06-10 07:58:43 +0000

    add-admins-test

---
+++ spec/system/orders_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'adminsログイン' do
+    let(:user) { create(:user) }
+    let(:admin) { create(:admin) }
+    let(:test_admin) { admin }
+    let!(:order) { create(:order, user: user) }
+    before do
+      visit new_admin_session_path
+      fill_in 'admin[email]', with: test_admin.email
+      fill_in 'admin[password]', with: test_admin.password
+      click_button 'ログイン'
+    end
+    context 'トップ画面に遷移' do
+      it '注文履歴一覧と表示される' do
+        expect(page).to have_content '注文履歴一覧'
+      end
+      it '注文履歴一覧のページ遷移に成功する' do
+        click_button '注文履歴一覧'
+        visit admins_orders_path
+        expect(page).to have_content '購入日時'
+      end
+    end
+  end
